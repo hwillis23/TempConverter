137bd0a18f0d2cb61ffb69e775bd91b189fdfad4
Hannah Willis <hwillis8817@gmail.com>
2022-04-01 11:47:16 -0500

    inital commit

---
+++ Temperature Conversion static Ex 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Temperature_Conversion_static_Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahTemp = 45.7;
+            double celsTemp = 15.2;
+
+
+            Console.WriteLine($" The temperature value in Fahrenheite is: {TempConverter.FahrenheitToCelsius(fahTemp)}");
+            Console.WriteLine();
+            Console.WriteLine($"The temperature value in Celsius is: {TempConverter.CelsiusToFahrenheit(celsTemp)}"); 
+
+        }
+    }
+}
---
+++ Temperature Conversion static Ex 1/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Temperature_Conversion_static_Ex_1
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahTemp)
+        {
+            return (fahTemp - 32) * 5.0 / 9.0;
+        }
+
+        public static double CelsiusToFahrenheit(double celsTemp)
+        {
+            return (celsTemp * 9.0 / 5.0) + 32;
+        }
+
+    }
+}
